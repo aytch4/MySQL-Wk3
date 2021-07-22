8cd121f289dbc45cd330ce765d7828369cddf660
Heather Staples <73347541+aytch4@users.noreply.github.com>
2021-07-22 13:43:39 -0400

    Add files via upload

--- Wk3-social_media_db.sql
+++ Wk3-social_media_db.sql
@@ -2,8 +2,8 @@ create database if not exists Social_Media_DB;
 
 use Social_Media_DB;
 
-drop table if exists posts;
 drop table if exists comments;
+drop table if exists posts;
 drop table if exists users;
 
 create table users (
@@ -18,7 +18,7 @@ create table users (
 );
 
 create table posts (
-	id int(11) not null auto_increment,
+	id int not null auto_increment,
 	users_username varchar(15) not null,
 	date_posted datetime default current_timestamp,
 	contents varchar(255) not null,
@@ -27,11 +27,11 @@ create table posts (
 );
 
 create table comments (
-	id int(11) not null auto_increment,
+	id int not null auto_increment,
 	users_username varchar(15) not null,
 	date_posted datetime default current_timestamp,
 	contents varchar(255) not null,
-	posts_id int(11) not null,
+	posts_id int not null,
 	primary key(id),
 	foreign key (posts_id) references posts(id),
 	foreign key (users_username) references users(username)
